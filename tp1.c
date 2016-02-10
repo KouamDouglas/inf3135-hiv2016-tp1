c21d6e71278602d4b7b767cc458e4777dcc9b8f9
kouokam kouam douglas <kouokam_kouam.douglas@courrier.uqam.ca>
2016-02-10 16:11:24 -0500

    declaration des constantes

---
+++ tp1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+#define MIN_SAISIE_CLAVIER 1
+#define MAX_SAISIE_CLAVIER 5000
+
+#define LONG_NOM_PAYS 100
+#define LONG_CODE_PAYS 50
+#define LONG_NOM_VILLE 100
+#define TAILLE_DU_BUFFER 3700
+#define TAILLE_TAB_PAYS 304
+#define TAILLE_TAB_VILLE 23493
