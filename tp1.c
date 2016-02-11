44de19cdf30bc605d108c7ba928e6a1e7accad58
kouokam kouam douglas <kouokam_kouam.douglas@courrier.uqam.ca>
2016-02-10 20:07:28 -0500

    debut du refactoring decoupage du devoir en fonct

--- tp1.c
+++ tp1.c
@@ -20,56 +20,36 @@ typedef struct Ville {
     long population;
     Pays pays;
 }Ville;
-
+ 
 static int compare_population_ville (void const *a, void const *b) {
     struct Ville const *pa = a;
     struct Ville const *pb = b;
     return ((int)pb->population - (int)pa->population);
 }
 
+void ouvertureFichier(FILE*,FILE*);
+void validationEntree(int);
+void testeDeParametre(int);
+void traitementDuFichierPays(FILE*,Pays[]);
+void traitementDuFichierVille(FILE*,Pays[],Ville[]);
+
 int main(int argc, char** argv){
-    FILE* fichierVille;
-    FILE* fichierPays;
-    if(argc!=2){
-        printf("Attention arguments invalides!!Le nombres d'arguments doit etre egal a 2 SVP!\n");
-        return 1;
-    }
+    FILE* fichierVille=fopen("cities15000.txt","r");
+    FILE* fichierPays=fopen("countryInfo.txt","r");
+    validationEntree(argc);
+    ouvertureFichier(fichierVille,fichierPays);
 
     long n;
     n=atol(argv[1]);
-    if(MIN_SAISIE_CLAVIER>n||n>MAX_SAISIE_CLAVIER){
-        printf("Attention!La valeur entree doit etre comprise entre 1 et 5000 SVP!\n");
-        return 1;
-    }     
-
-    fichierVille=fopen("cities15000.txt","r");
-    fichierPays=fopen("countryInfo.txt","r");    
+    testeDeParametre(n);     
+   
     if(!fichierVille||!fichierPays){
         printf("Veuillez verifier les fichiers!\n");
     }else{
-        Pays paysTab[TAILLE_TAB_PAYS];
+        Pays paysTabValidation[TAILLE_TAB_PAYS];
         char buffer[TAILLE_DU_BUFFER];
-        char *token, *ps;
-        int remplissage=0;
-	while ( fgets(buffer,TAILLE_DU_BUFFER,fichierPays) ){
-            if (buffer[0]=='#') {
-                continue;
-            }
-            Pays paysTempo;
-            int compteur = 0;
-            ps = buffer;
-            while ((token = strsep(&ps, "\t")) != NULL) {
-                if(compteur == 0){
-                    strcpy(paysTempo.code, token);
-                }
-                if(compteur == 4 ){
-                    strcpy(paysTempo.nom, token);
-                }
-                compteur++;
-            }
-            paysTab[remplissage] = paysTempo;
-            remplissage++;
-        }
+       
+        traitementDuFichierPays(fichierPays,paysTabValidation);
 	
 	Ville villeTab[TAILLE_TAB_VILLE];
         char bufferVille[TAILLE_DU_BUFFER];
@@ -94,8 +74,8 @@ int main(int argc, char** argv){
                     strcpy(correspondance, tokenVille);
                     int c=0;
                     for (c=0; c<TAILLE_TAB_PAYS; c++) {
-                        if (strcmp(paysTab[c].code,correspondance)==0) {
-                            villeTempo.pays=paysTab[c];
+                        if (strcmp(paysTabValidation[c].code,correspondance)==0) {
+                            villeTempo.pays=paysTabValidation[c];
                         }
                     }
                 }
@@ -118,5 +98,51 @@ int main(int argc, char** argv){
     fclose(fichierPays);
     }
     return 0;
+}
+
+void ouvertureFichier(FILE* fichierVille,FILE* fichierPays){
+    if(!fichierVille||!fichierPays){
+        printf("Veuillez verifier les fichiers 1!\n");
+        exit(1);     
+    }
+}
+
+void validationEntree(int argc){
+    if(argc!=2){
+        printf("Attention arguments invalides!!Le nombres d'arguments doit etre egal a 2 SVP!\n");
+        exit(1);
+    }   
+}
+
+void testeDeParametre(int nombreDeVille){
+    if(MIN_SAISIE_CLAVIER>nombreDeVille||nombreDeVille>MAX_SAISIE_CLAVIER){
+        printf("Attention!La valeur entree doit etre comprise entre 1 et 5000 SVP!\n");
+        exit(1);
+    }
+}
+
+void traitementDuFichierPays(FILE* fichierPays,Pays paysTab[]){
+    char buffer[TAILLE_DU_BUFFER];
+    char *token, *ps;
+    int remplissage=0;
+    while ( fgets(buffer,TAILLE_DU_BUFFER,fichierPays) ){
+            if (buffer[0]=='#') {
+                continue;
+            }
+            Pays paysTempo;
+            int compteur = 0;
+            ps = buffer;
+            while ((token = strsep(&ps, "\t")) != NULL) {
+                if(compteur == 0){
+                    strcpy(paysTempo.code, token);
+                }
+                if(compteur == 4 ){
+                    strcpy(paysTempo.nom, token);
+                }
+                compteur++;
+            }
+            paysTab[remplissage] = paysTempo;
+            remplissage++;
+        }
 }
 
